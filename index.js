147f43572ffd42b3e193b46e3c617f0812341726
Pauline Muigai <wanjirumuigai@gmail.com>
2022-11-30 18:19:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(String="roller-skate") {
+    return `This Saturday, I want to ${String}!`;
+}
+
+saturdayFun();
+
+function mondayWork(work="go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+
+function wrapAdjective(msg='*') {
+    return function inner(String="special") {
+        return `You are ${msg}${String}${msg}!`
+    }
+}
+
+wrapAdjective()();
